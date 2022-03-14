c43cd743ebd32d1af0dc3c6d361dbb14f45d4288
David Ohayon <davidohayon@turtlefactoryinc.com>
2022-03-14 18:16:25 +0000

    Initial Commit

---
+++ Timly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timly
+//
+//  Created by David Ohayon Jr. on 14/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timly/TimlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimlyApp.swift
+//  Timly
+//
+//  Created by David Ohayon Jr. on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
